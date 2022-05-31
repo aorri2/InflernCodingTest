a262573aa508dc154f5240ce3f9021ec0feaedfb
aorri2 <76669404+aorri2@users.noreply.github.com>
2022-05-31 13:39:46 +0900

    09번, 11번 중복되는 문자 갯수 출력 문제 풀이.

---
+++ src/inflern01_11.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class inflern01_11 {
+
+    public static String solution(String str){
+        String answer = "";
+        int cnt = 1;
+        str = str + " ";
+        for(int i=0; i<str.length()-1;i++){
+            if(str.charAt(i) == str.charAt(i+1)){
+                cnt++;
+            }else{
+                answer+=str.charAt(i);
+                if(cnt>1) answer+=String.valueOf(cnt);
+                cnt = 1;
+            }
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(solution(str));
+    }
+}
